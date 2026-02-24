0bc85efc4f04346a36640e3baf8760ea77daa1dd
sebgo <sebgon02@gmail.com>
2026-02-24 13:11:08 +0100

    Added a function that converts Farenheit to celsius

--- SprintDemo/Program.cs
+++ SprintDemo/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Console.WriteLine("===TEMPERATUR OMVANLDARE===");
+
+Console.Write("Skriv in temperaturen i Farenheit:");
+int inTemp = Int32.Parse(Console.ReadLine());
+
+ static int FarToCels(int startTemp)
+{
+    var convertedTemp = (startTemp - 32) * (5 / 9);
+
+    return convertedTemp;
+}
+
+
+Console.Write("Tempen i Celsius är: ");
+Console.WriteLine(FarToCels(inTemp));

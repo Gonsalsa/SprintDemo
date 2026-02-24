580ad348dd1547ebd26d67e00676d1b45a59b1e1
Christian <savic>
2026-02-24 13:09:45 +0100

    Done code

--- SprintDemo/Program.cs
+++ SprintDemo/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more informationc
+
+
+
+Console.WriteLine("Enter the temperature in Farenheit:");
+int tempCelsius = Convert.ToInt32(Console.ReadLine());
+var farenheit = tempCelsius * 1.8 + 32;
+
+Console.WriteLine($"{farenheit}");
+

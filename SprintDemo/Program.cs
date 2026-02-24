a50e9932577f34228ed35e0a7fe8a8197db795ca
Christian <savic>
2026-02-24 13:19:36 +0100

    Added a switch

--- SprintDemo/Program.cs
+++ SprintDemo/Program.cs
@@ -1,10 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more informationc
+﻿Console.WriteLine("Choose your conversion:");
+Console.WriteLine("1: Convert Fahrenheit to Celsius");
+Console.WriteLine("2: Convert Celsius to Fahrenheit");
 
+int choice = Convert.ToInt32(Console.ReadLine());
 
+switch (choice)
+{
+   case 1:
+      Console.Write("Enter the temperature in Fahrenheit: ");
+      double fahrInput = Convert.ToDouble(Console.ReadLine());
+      
+      double toCelsius = (fahrInput - 32) / 1.8;
+        
+      Console.WriteLine($"{fahrInput}°F is {toCelsius:N2}°C");
+      break;
 
-Console.WriteLine("Enter the temperature in Farenheit:");
-int tempCelsius = Convert.ToInt32(Console.ReadLine());
-var farenheit = tempCelsius * 1.8 + 32;
+   case 2:
+      Console.Write("Enter the temperature in Celsius: ");
+      double celsInput = Convert.ToDouble(Console.ReadLine());
+      
+      double toFahrenheit = (celsInput * 1.8) + 32;
+        
+      Console.WriteLine($"{celsInput}°C is {toFahrenheit:N2}°F");
+      break;
 
-Console.WriteLine($"{farenheit}");
-
+   default:
+      Console.WriteLine("Invalid choice. Please pick 1 or 2.");
+      break;
+}
